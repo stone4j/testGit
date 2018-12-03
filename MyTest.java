49bcd15672ab8d83d45e9375f369aa70ac207783
stone4j <139310186@qq.com>
2018-12-03 16:29:10 +0800

    yinggai buhuiyou chongtu

--- MyTest.java
+++ MyTest.java
@@ -6,13 +6,13 @@ public class MyTest
 
 		System.out.println("hello fsadf git sault");
 		System.out.println("hello fsadf git sault");
-		System.out.println("hello fsadf git sault");
-
+		
 		System.out.println("hello sault git right way ");
 
 		//System.out.println("hello git sault");
 
 
+		System.out.println("hello git");
 		System.out.println("hello git");
 		System.out.println("hello git");
 	}
