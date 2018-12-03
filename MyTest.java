da3273667b9d18785da1d9943714b84511d407a0
stone4j <139310186@qq.com>
2018-12-03 16:10:12 +0800

    second conflict

--- MyTest.java
+++ MyTest.java
@@ -3,7 +3,7 @@ public class MyTest
 {
 	public static void main(String[] arg)
 	{
-		System.out.println("hello git sault");
+		System.out.println("hello fsadf git sault");
 		System.out.println("hello git");
 		System.out.println("hello git");
 	}
