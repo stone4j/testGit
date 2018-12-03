b4e167b1d6c085d496e8cbf36684e601f8dc5c5d
stone4j <139310186@qq.com>
2018-12-03 15:16:59 +0800

    push code to git

---
+++ MyTest.java
@@ -0,0 +1,10 @@
+
+public class MyTest
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("hello git");
+		System.out.println("hello git");
+		System.out.println("hello git");
+	}
+}
