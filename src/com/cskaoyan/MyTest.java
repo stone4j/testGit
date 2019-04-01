a5e70590a58d1245a47184fefd4c698a452733d4
stone4j <shitoushaoxia1993@gmail.com>
2019-04-02 00:48:55 +0800

    提交

--- src/com/cskaoyan/MyTest.java
+++ src/com/cskaoyan/MyTest.java
@@ -4,8 +4,6 @@ public class MyTest {
     public static void main(String[] ar){
         System.out.println("hello idea git");
         System.out.println("hello idea git");
-        System.out.println("hello idea git");
-        System.out.println("hello idea git");
         new MyTest().newMethod();
     }
     private void newMethod(){
