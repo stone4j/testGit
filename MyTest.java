da025796d053df9aa9f5cb429946b0e22ecb33d8
stone4j <shitoushaoxia1993@gmail.com>
2019-04-01 23:40:52 +0800

    repo1 commit 2

--- MyTest.java
+++ MyTest.java
@@ -4,10 +4,10 @@ public class MyTest
 	public static void main(String[] arg)
 	{
 
-		System.out.println("hello fsadf git sault");
-		System.out.println("hello fsadf git sault");
+
+	
 		
-		System.out.println("hello sault git right way ");
+
 
 		//System.out.println("hello git sault");
 
