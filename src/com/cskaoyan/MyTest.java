e86cf5545ebd85d8dd66acca40dc7598f03a47cc
stone4j <shitoushaoxia1993@gmail.com>
2019-04-02 00:05:09 +0800

    idea commit

--- src/com/cskaoyan/MyTest.java
+++ src/com/cskaoyan/MyTest.java
@@ -3,6 +3,9 @@ package com.cskaoyan;
 public class MyTest {
     public static void main(String[] ar){
         System.out.println("hello idea git");
+        System.out.println("hello idea git");
+        System.out.println("hello idea git");
+        System.out.println("hello idea git");
         new MyTest().newMethod();
     }
     private void newMethod(){
