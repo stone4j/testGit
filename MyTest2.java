5a00bfee1ae1adebdfc7e4df86decdc28d18c125
stone4j <139310186@qq.com>
2019-04-02 11:54:27 +0800

    test2 commmit by repo2

--- MyTest2.java
+++ MyTest2.java
@@ -3,8 +3,10 @@ public class MyTest
 {
 	public static void main(String[] arg)
 	{
+		System.out.println("hello repo2");
 		System.out.println("hello git");
 		System.out.println("hello git");
 		System.out.println("hello git");
 	}
+	
 }
