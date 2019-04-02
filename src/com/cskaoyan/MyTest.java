20cc75bf478765bc66a5307cb43c6552f54c7f4d
stone4j <shitoushaoxia1993@gmail.com>
2019-04-02 15:03:08 +0800

    发现冲突

--- src/com/cskaoyan/MyTest.java
+++ src/com/cskaoyan/MyTest.java
@@ -5,7 +5,7 @@ public class MyTest {
         System.out.println("hello idea git");
         System.out.println("hello idea git");
         new MyTest().newMethod();
-        System.out.println("this is update by 0402");
+        System.out.println("this is update by idea_repo");
     }
     private void newMethod(){
         System.out.println("fine 3q");
