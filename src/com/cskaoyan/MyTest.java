7de19236022fc1442e3d68598d8be9b1a6112a77
stone4j <shitoushaoxia1993@gmail.com>
2019-04-02 15:13:37 +0800

    from terminal

--- src/com/cskaoyan/MyTest.java
+++ src/com/cskaoyan/MyTest.java
@@ -7,6 +7,7 @@ public class MyTest {
         new MyTest().newMethod();
         System.out.println("this is repo3 by 0402");
         System.out.println("this is update by idea_repo");
+
     }
     private void newMethod(){
         System.out.println("fine 3q");
